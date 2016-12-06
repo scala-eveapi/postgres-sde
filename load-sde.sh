0b42b30ed3cff1835fc84ee1e98a90bb0b678797
Simon Hafner <hafnersimon@gmail.com>
2016-12-06 17:21:48 +0100

    Create yaml and drop afterwards for dump

--- load-sde.sh
+++ load-sde.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 set -e
 
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+    CREATE USER yaml;
+EOSQL
+
 pg_restore -d "${POSTGRES_DB:-$POSTGRES_USER}" -U "$POSTGRES_USER" sde
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+    REASSIGN OWNED BY yaml TO $POSTGRES_USER;
+    DROP OWNED BY yaml;
+EOSQL
