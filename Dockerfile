79ee1ee2322537f99fa84fb740f5e578022618e9
Simon Hafner <hafnersimon@gmail.com>
2016-12-06 17:38:35 +0100

    Proper download link

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,5 @@
 FROM postgres:latest
-ADD https://www.fuzzwork.co.uk/dump/latest/postgres-20161114-TRANQUILITY.dmp.bz2 sde.bz2
-# ADD postgres-20161114-TRANQUILITY.dmp.bz2 sde.bz2
-RUN bunzip2 sde.bz2
+ADD https://www.fuzzwork.co.uk/dump/postgres-latest.dmp.bz2 postgres-latest.dmp.bz2
+RUN bunzip2 postgres-latest.dmp.bz2
 COPY load-sde.sh /docker-entrypoint-initdb.d/01-load-sde.sh
 COPY add-constraints.sql /docker-entrypoint-initdb.d/02-add-constraints.sql
--- load-sde.sh
+++ load-sde.sh
@@ -3,9 +3,14 @@ set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     CREATE USER yaml;
+    GRANT ALL ON ALL TABLES IN SCHEMA public TO yaml;
 EOSQL
 
-pg_restore -d "${POSTGRES_DB:-$POSTGRES_USER}" -U "$POSTGRES_USER" sde
+echo "starting restore"
+
+pg_restore -d "${POSTGRES_DB:-$POSTGRES_USER}" -U "$POSTGRES_USER" postgres-latest.dmp
+
+echo "restore done"
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     REASSIGN OWNED BY yaml TO $POSTGRES_USER;
